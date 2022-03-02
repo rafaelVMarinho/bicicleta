618484b034014a758770be8e7aa0df93f7a0550f
rafaelVMarinho <98613474+rafaelVMarinho@users.noreply.github.com>
2022-03-02 17:01:25 -0300

    Add files via upload

--- Project-Template-C17-main/sketch.js
+++ Project-Template-C17-main/sketch.js
@@ -202,7 +202,7 @@ function redCyclists(){
 //  distance = 0;
 // }
 
-function reset() {
+/*function reset() {
   gameState = PLAY;
   gameOver.visible = true;
   mainCyclist.addAnimation("SahilRunning",mainRacerImg1);
@@ -212,19 +212,19 @@ function reset() {
   redCG.destroy();
   
   distance = 0;
-}
+}*/
 
-//function reset(){
-//  gameState = PLAY;
-//  gameOver.visible = false;
-//  mainCyclist.addAnimation("SahilRunning",mainRacerImg1);
+function reset(){
+  gameState = PLAY;
+  gameOver.visible = false;
+  mainCyclist.addAnimation("SahilRunning",mainRacerImg1);
   
-//  pinkCG.destroyEach();
-//  yellowCG.destroyEach();
-//  redCG.destroyEach();
+  pinkCG.destroyEach();
+  yellowCG.destroyEach();
+  redCG.destroyEach();
   
-//  distance = 0;
-// }
+  distance = 0;
+ }
 
 //function reset(){
 //  gameState = END;
